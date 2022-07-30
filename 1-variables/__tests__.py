511e8ddd99f10e4dad2bc7e4cfa8d6c41ab1b38d
Jean Sainctavit <jean.sainctavit@epita.fr>
2022-07-30 13:38:22 +0200

    add corrected exercises on variables

---
+++ 1-variables/__tests__.py
@@ -0,0 +1,69 @@
+from io import StringIO
+import unittest
+import sys
+import re
+
+mystdout = StringIO()
+sys.stdout = mystdout
+
+import variables
+
+sys.stdout = sys.__stdout__
+printed = mystdout.getvalue().splitlines()
+
+
+class VariablesTest(unittest.TestCase):
+
+    def assertVariablesHasattr(self, name):
+        errmsg = f"variable {name} does not exist"
+        self.assertTrue(hasattr(variables, name), errmsg)
+
+    def assertType(self, var, expected_type):
+        errmsg = f"variable {var} does not have type {expected_type}"
+        self.assertIs(type(var), expected_type, errmsg)
+
+    def test_age(self):
+        self.assertVariablesHasattr("age")
+        self.assertType(variables.age, int)
+
+    def test_name(self):
+        self.assertVariablesHasattr("name")
+        self.assertType(variables.name, str)
+
+    def test_size(self):
+        self.assertVariablesHasattr("size")
+        self.assertType(variables.size, float)
+
+    def test_food(self):
+        self.assertVariablesHasattr("likes_apples")
+        self.assertVariablesHasattr("likes_pasta")
+        self.assertType(variables.likes_apples, bool)
+        self.assertType(variables.likes_pasta, bool)
+        self.assertTrue(variables.likes_apples or variables.like_pasta)
+
+    def test_neg(self):
+        self.assertVariablesHasattr("neg")
+        self.assertType(variables.neg, int)
+        self.assertTrue(variables.neg < 0)
+
+    def test_print_age(self):
+        for v in printed:
+            if (v.isdecimal()):
+                return
+        self.assertTrue(False)
+
+    def test_print_likes_apples(self):
+        for v in printed:
+            if (v == "True" or v == "False"):
+                return
+        self.assertTrue(False)
+
+    def test_print_neg(self):
+        for v in printed:
+            if (re.match('^-[0-9]+$', v)):
+                return
+        self.assertTrue(False)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ 1-variables/variables.py
@@ -0,0 +1,35 @@
+# Add a comment
+
+
+# This is a comment
+
+
+# Add a multiline comment
+
+
+# Create an integer variable named "age", and store your age in it
+
+
+# Create a string varaible named "name", and store your name in it
+
+
+# Create a float variable named "size", and store your size in it
+
+
+# Create two boolean variables, named "likes_apples" and "likes_pasta"
+# One should be True, and the other should be False
+
+
+# Create an integer named "neg", and store a negative value inside of it
+
+
+# Change the value of "neg" to store another negative value inside of it
+
+
+# Now, print the variable "age"
+
+
+# Then, print print the variable "likes_apples"
+
+
+# Lastly, print the variable "neg"
